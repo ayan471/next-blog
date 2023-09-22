9d0bbd7de759a68a1772ab375082e95505f0d35a
ayan471 <pakhiraayan@gmail.com>
2023-09-22 16:06:27 +0530

    loading the comments

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,29 +1,31 @@
-import React from "react";
-import styles from "./card.module.css";
 import Image from "next/image";
+import styles from "./card.module.css";
 import Link from "next/link";
 
-const Card = () => {
+const Card = ({ key, item }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-      </div>
+    <div className={styles.container} key={key}>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>21.09.2023 - </span>
-          <span className={styles.category}>CULTURE</span>
+          <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{" "}
+          </span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href="/">
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <Link href={`/posts/${item.slug}`}>
+          <h1>{item.title}</h1>
         </Link>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel eos
-          facilis eveniet expedita praesentium laboriosam perspiciatis incidunt
-          vitae similique nesciunt aperiam, ipsa nobis atque esse. Distinctio
-          laborum doloribus placeat facere?
-        </p>
-        <Link href="/" className={styles.link}>
+        {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
+        <div
+          className={styles.desc}
+          dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 60) }}
+        />
+        <Link href={`/posts/${item.slug}`} className={styles.link}>
           Read More
         </Link>
       </div>
